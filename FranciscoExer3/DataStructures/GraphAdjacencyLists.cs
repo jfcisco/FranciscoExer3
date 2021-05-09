e9e4a213add0f66f7ffd319334b9e96b7021b0a4
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-09 15:21:24 +0800

    Implement AdjacencyList indexer property

--- FranciscoExer3/DataStructures/GraphAdjacencyLists.cs
+++ FranciscoExer3/DataStructures/GraphAdjacencyLists.cs
@@ -13,6 +13,16 @@ namespace FranciscoExer3.DataStructures
     {
         private ListNode<T> Root;
 
+        public GraphAdjacencyLists() { }
+
+        public GraphAdjacencyLists(T[] vertices)
+        {
+            foreach(T vertex in vertices)
+            {
+                this[vertex] = new LinkedList<T>();
+            }
+        }
+
         // Get => Returns the adjacencyList of the given vertex
         // Set => Updates the adjacencyList of the given vertex
         public LinkedList<T> this[T vertex]
@@ -26,18 +36,21 @@ namespace FranciscoExer3.DataStructures
         // Vertex - vertex whose adjacency list is requested
         private LinkedList<T> GetAdjacencyList(ListNode<T> root, T vertex)
         {
-            if (root == null)
+            int? orderValue = root?.Key.CompareTo(vertex);
+            // orderValue < 0       if vertex > root.Key
+            // orderValue == 0      if vertex == root.Key
+            // orderValue > 0       if vertex < root.Key
+            // ordeValue == null    if root == null
+
+            if (orderValue == null)
             {
                 throw new ArgumentOutOfRangeException($"Vertex {vertex} does not have an adjacency list.");
             }
-
-            // Traverse the Binary Search Tree
-            int orderValue = root.Key.CompareTo(vertex);
-            if (orderValue < 0)
+            else if (orderValue > 0)
             {
                 return GetAdjacencyList(root.Left, vertex);
             }
-            else if (orderValue > 0)
+            else if (orderValue < 0)
             {
                 return GetAdjacencyList(root.Right, vertex);
             }
@@ -52,41 +65,45 @@ namespace FranciscoExer3.DataStructures
         // Vertex - vertex whose adjacency list will be set to adjacencyList
         private void SetAdjacencyList(ref ListNode<T> root, T vertex, LinkedList<T> adjacencyList)
         {
-            // Find the order of root.Key compared to vertex
+            // Find the relative order of root.Key to vertex
             int? orderValue = root?.Key.CompareTo(vertex);
+            // orderValue < 0       if vertex > root.Key
+            // orderValue == 0      if vertex == root.Key
+            // orderValue > 0       if vertex < root.Key
+            // ordeValue == null    if root == null
 
+            // Cases: Vertex does not exist in the tree, or Vertex is already in the tree
             if (orderValue == null || orderValue == 0)
             {
                 // Add new ListNode to this position
-                root = new ListNode<T> 
+                root = new ListNode<T>(vertex)
                 { 
-                    Key = vertex, 
                     AdjacencyList = adjacencyList
                 };
                 return;
             }
             // Traverse the Binary Search Tree
-            else if (orderValue < 0)
+            else if (orderValue > 0)
             {
                 SetAdjacencyList(ref root.Left, vertex, adjacencyList);
             }
-            else if (orderValue > 0)
+            else if (orderValue < 0)
             {
                 SetAdjacencyList(ref root.Right, vertex, adjacencyList);
             }
         }
 
-        private void Add(ref ListNode<T> node, T vertex, T adjacentVertex)
+        private class ListNode<U>
         {
+            public U Key;
+            public LinkedList<U> AdjacencyList;
+            public ListNode<U> Left;
+            public ListNode<U> Right;
 
-        }
-
-        private class ListNode<T>
-        {
-            public T Key;
-            public LinkedList<T> AdjacencyList;
-            public ListNode<T> Left;
-            public ListNode<T> Right;
+            public ListNode(U key)
+            {
+                Key = key;
+            }
         }
     }
 }
