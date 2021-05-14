bf5b6b3390b3dd6d445026d8223328eaa4d46737
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-14 10:09:27 +0800

    Add Queue unit tests to help debug

---
+++ FranciscoExer3.Tests/QueueTests.cs
@@ -0,0 +1,46 @@
+﻿using FranciscoExer3.DataStructures;
+using Xunit;
+
+namespace FranciscoExer3.Tests
+{
+    public class QueueTests
+    {
+        private Queue<int> TestQueue;
+
+        public QueueTests()
+        {
+            TestQueue = new Queue<int>();
+        }
+
+        [Fact]
+        public void InstantiateQueue_Success()
+        {
+            Assert.NotNull(TestQueue);
+        }
+
+        [Fact]
+        public void Enqueue_Success()
+        {
+            TestQueue.Enqueue(1);
+            TestQueue.Enqueue(2);
+            TestQueue.Enqueue(3);
+
+            Assert.Equal(3, TestQueue.Count);
+        }
+
+        [Fact]
+        public void Dequeue_Success()
+        {
+            TestQueue.Enqueue(1);
+            TestQueue.Enqueue(2);
+            TestQueue.Enqueue(3);
+
+            int[] expectedDequeues = { 1, 2, 3 };
+            
+            foreach(int i in expectedDequeues)
+            {
+                Assert.Equal(i, TestQueue.Dequeue());
+            }
+        }
+    }
+}
