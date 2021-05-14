fb1e092e2e19839e207dcc37e99a3b673c98ec62
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-14 10:09:19 +0800

    Add own Queue and Stack implementations

---
+++ FranciscoExer3/DataStructures/Queue.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FranciscoExer3.DataStructures
+{
+    /// <summary>
+    /// Queue data structure implemented using an array that can be resized when needed.
+    /// </summary>
+    public class Queue<T>
+    {
+        public T[] Data { get; set; } = new T[1];
+        private int Head = 0; // Size - 1
+        private int Tail = 0; // Size - 1
+        private int Size = 1;
+
+        public int Count { get; private set; } = 0;
+
+        public Queue() { }
+
+        /// <summary>
+        /// Inserts an item at the back of the queue.
+        /// </summary>
+        /// <param name="dataToInsert"></param>
+        public void Enqueue(T dataToEnqueue)
+        {
+            // Check if the Queue is already full
+            if (Count == Size)
+            {
+                GrowArray();
+            }
+
+            // Insert at the index where Tail is currently pointing to
+            Data[Tail] = dataToEnqueue;
+            Count++;
+
+            // Circle back when end of array is reached
+            if (Tail == Size - 1)
+            {
+                Tail = 0;
+            }
+            else
+            {
+                Tail++;
+            }
+        }
+
+        /// <summary>
+        /// Returns and removes the item at the front of the queue.
+        /// </summary>
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("Queue underflow.");
+            }
+
+            // Temporarily store data to be returned later on, then set Data at Head to default value
+            T dataAtHead = Data[Head];
+            Data[Head] = default;
+
+            // Move head to next item
+            if (Head == Size - 1)
+            {
+                Head = 0;
+            }
+            else
+            {
+                Head++;
+            }
+
+            Count--;
+            return dataAtHead;
+
+        }
+
+        /// <summary>
+        /// Procedure that increases the size of the queue when the Data array is already full.
+        /// </summary>
+        /// <param name="newSize"></param>
+        private void GrowArray()
+        {
+            // Create a bigger data array
+            T[] newArray = new T[Size * 2];
+
+            // Copy the contents from the old array to the new array
+            // Case 1 & 2: Array is empty or only partially filled (GrowArray should never be called in these cases)
+            // Case 3: Array is filled (i.e. Head = Tail)
+
+            int counter = 0;
+            // Copy all elements from Head to end of Data array
+            for (var i = 0; i < Size - Head; i++)
+            {
+                // Data[Head...Size - 1] => newArray[0...Size - 1 - Head]
+                newArray[i] = Data[i + Head];
+                counter++;
+            }
+
+            // Copy all elements from 0 to Tail
+            for (var j = 0; j < Tail; j++)
+            {
+                // Data[0...Tail] => newArray[counter...counter + Tail]
+                newArray[j + counter] = Data[j];
+                counter++;
+            }
+
+            // Update the Queue properties to the newArray
+            Size *= 2;
+            Data = newArray;
+            Head = 0; // Item at the first inserted item
+            Tail = counter; // Index of the next empty spot
+        }
+    }
+}
---
+++ FranciscoExer3/DataStructures/Stack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FranciscoExer3.DataStructures
+{
+    /// <summary>
+    /// Implements the Stack data structure using arrays
+    /// </summary>
+    /// <typeparam name="T">Type of stack elements</typeparam>
+    internal class Stack<T>
+    {
+        private int Size = 1;
+        private T[] Data = new T[1];
+        private int Head = 0;
+
+        public Stack() { }
+
+        /// <summary>
+        /// Counts how many items are currently in the stack
+        /// </summary>
+        public int Count => Head;
+
+        /// <summary>
+        /// Pushes an item to the stack
+        /// </summary>
+        /// <param name="item">Item to be pushed</param>
+        public void Push(T item)
+        {
+            if (Head == Size)
+            {
+                // No more space in the stack
+                int newSize = Size * 2;
+                Data = GrowArray(newSize);
+                Size = newSize;
+            }
+
+            Data[Head] = item;
+            Head++;
+        }
+
+        /// <summary>
+        /// Helper method to grow the Data array to avoid overflowing the stack.
+        /// </summary>
+        /// <param name="newSize"></param>
+        /// <returns>The Data array resized to newSize</returns>
+        private T[] GrowArray(int newSize)
+        {
+            T[] newArray = new T[newSize];
+
+            // Copy Data elements to bigger array
+            for (int i = 0; i < Size; i++)
+            {
+                newArray[i] = Data[i];
+            }
+
+            return newArray;
+        }
+
+        /// <summary>
+        /// Returns and "deletes" the last pushed item from the stack
+        /// </summary>
+        /// <returns>The last pushed item from the stack</returns>
+        public T Pop()
+        {
+            if (Head == 0)
+            {
+                throw new Exception("Stack underflow error!");
+            }
+
+            Head--;
+            return Data[Head];        
+        }
+    }
+}
