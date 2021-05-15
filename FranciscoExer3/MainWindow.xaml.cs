f13185e25e026cf5e649db07145c46558f0422ec
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-15 14:27:00 +0800

    Add initial window design

--- FranciscoExer3/MainWindow.xaml.cs
+++ FranciscoExer3/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace FranciscoExer3
 {
@@ -23,6 +10,53 @@ namespace FranciscoExer3
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DepthTraversalMenu_Click(object sender, RoutedEventArgs e)
+        {
+            // Open DFT Context Menu
+            MenuDFT.IsOpen = true;
+        }
+
+        private void DepthTraversalGraph1_Click(object sender, RoutedEventArgs e)
+        {
+            // Not yet implemented
+        }
+
+        private void DepthTraversalGraph2_Click(object sender, RoutedEventArgs e)
+        {
+            // Not yet implemented
+        }
+
+        private void BreadthTraversalMenu_Click(object sender, RoutedEventArgs e)
+        {
+            // Open BFT Context Menu
+            MenuBFT.IsOpen = true;
+        }
+
+        private void BreadthTraversalGraph1_Click(object sender, RoutedEventArgs e)
+        {
+            // Not yet implemented
+        }
+
+        private void BreadthTraversalGraph2_Click(object sender, RoutedEventArgs e)
+        {
+            // Not yet implemented
+        }
+
+        private void SearchGraph1_Click(object sender, RoutedEventArgs e)
+        {
+            // Not yet implemented
+        }
+
+        private void SearchGraph2_Click(object sender, RoutedEventArgs e)
+        {
+            // Not yet implemented
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Given two predefined graphs, the program allows the user to perform depth-first 
 
 ## Implementation Status of Requirements
 
-- [ ] The program displays a window with a menu of 5 items enumerated below.
+- [~] The program displays a window with a menu of 5 items enumerated below.
 - [ ] [1] Perform Depth First Traversal 
 	- When the user chooses [1], the user is asked which graph he or she wants to traverse. The program then prints out the final order in which the nodes are visited using the Depth First Traversal. Traversal will always start from node A or 0.
 - [ ] [2] Perform Breadth Fist Traversal 
@@ -13,6 +13,6 @@ Given two predefined graphs, the program allows the user to perform depth-first 
 	- Choosing [3] searches for the vertex inputted by the user. Use the DFS on Graph 1 and print a message if the vertex is not part of the graph.
 - [ ] [4] Search Graph 2(BFS)
 	- Choosing [4] searches for the vertex inputted by the user. Use the BFS on Graph 2 and print a message if the vertex is not part of the graph.
-- [ ] [5] Exit
+- [X] [5] Exit
 	- Keep on displaying the menu until the user chooses [5].
 - [X] Implement the Graph data structure using either an adjaceny matrix or **adjacency list**. Author's note: I'm choosing to do the latter implementation.
