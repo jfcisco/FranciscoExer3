0e0b50ebdb92fd17b86b13584a94ffb9ea0df131
Jasper Francisco <jasper.francisco@gmail.com>
2021-05-23 18:12:24 +0800

    Add brief description

--- FranciscoExer3/App.xaml.cs
+++ FranciscoExer3/App.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+
+/* Name: Jasper Lorenzo M. Francisco
+ * Student Number: 2020-30007
+ * Date: May 24, 2021
+ * Description of the Program: 
+ *      This application demonstrates the depth-first search and traversal,
+ *      and the breadth-first search and traversal methods of the graph
+ *      data structure. A graphical user interface is provided with options 
+ *      to perform depth-first traversal and breadth-first traversal for
+ *      one of the two predefined graphs, depth-first search for the first
+ *      graph, and breadth-first search for the second graph.
+ */
 
 namespace FranciscoExer3
 {
